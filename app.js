80679cf9263b399087f27822a232d8936dbd4cd6
dobby888 <pabbathisreevalli1705@gmail.com>
2023-11-15 08:03:38 +0530

    starting with express.js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require("http");
 
-const route = require("./route");
+const express = require("express");
 
-console.log(route.someText);
+const app = express();
 
-const server = http.createServer(route.handler);
+//use middleware after creating app object but before we pass it to create a server
+app.use((req, res, next) => {
+    console.log("in the missle ware");
+    next();//call next here(only if we dont send a response) allows the req to travel on to the next middleware in line if not the funciton just dies and cant continue to the next middleware
+});//add  a new middleware function:accepts an array of request handlers, third argument is a function: this function has to be executed to allow the request to travel on net middleware
 
-server.listen(9000);
+app.use((req, res, next) => {
+    console.log("in the another missle ware");
+    res.send("<h1>Hello From Express.</h1>");//sends the response by attatching a body of any kind
+
+});
+
+// const server = http.createServer(app);
+
+// server.listen(9000);
+
+app.listen(3000);//listens along with creating the server
