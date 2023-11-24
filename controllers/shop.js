c379ef7cc444f13fecd6c45c4b70b0239601cc4a
dobby888 <pabbathisreevalli1705@gmail.com>
2023-11-25 02:13:44 +0530

    retrieving data from databases

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,34 +2,41 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+        });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.findById(prodId)
+    .then(([product]) => {
+      //console.log(product)//the output when details is clicked is an array=>use [0] to obtain the details of the product
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(() => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+          prods: rows,
+          pageTitle: 'Shop',
+          path: '/'
+        });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
